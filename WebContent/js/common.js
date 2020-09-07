38663e4d2518754b5b51fa549ca9c1550f72f205
남아현 <ska6371@naver.com>
2020-09-07 19:11:39 +0900

    비밀번호 확인 작성 후에 비밀번호 작성했을 때 text 안 뜨는 오류 수정

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -6,17 +6,25 @@ function loginCheck(){
 }
 
 function pwCheck(){
-	$("#pw_check").on("propertychange change keyup paste input", ()=>{
-		if($('#pw_check').val() == ''){
-			$('#isPwTrue').text('');
-		} else if($('#pw').val() != $('#pw_check').val()){
-			$('#isPwTrue').css('color', 'red');
-			$('#isPwTrue').text('비밀번호가 틀렸습니다. 다시 입력해주세요.');
-		} else{
-			$('#isPwTrue').css('color', 'green');
-			$('#isPwTrue').text('옳은 비밀번호 입니다.');
-		}
+	$("#pw").on("propertychange change keyup paste input", ()=>{
+		pwchecking();
 	})
+
+	$("#pw_check").on("propertychange change keyup paste input", ()=>{
+		pwchecking();
+	})
+}
+
+function pwchecking(){
+	if($('#pw_check').val() == ''){
+		$('#isPwTrue').text('');
+	} else if($('#pw').val() != $('#pw_check').val()){
+		$('#isPwTrue').css('color', 'red');
+		$('#isPwTrue').text('비밀번호가 틀렸습니다. 다시 입력해주세요.');
+	} else{
+		$('#isPwTrue').css('color', 'green');
+		$('#isPwTrue').text('옳은 비밀번호 입니다.');
+	}
 }
 
 function alreadyLogin(){
