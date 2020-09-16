e018502ac29d6a77eab464bc70404f84c43966f7
남아현 <ska6371@naver.com>
2020-09-16 19:42:26 +0900

    마우스오버 할 때 해당 div 확대

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,3 +1,23 @@
+function zoomIn(event){
+	event.target.style.transform = "scale(1.1)";
+    event.target.style.zIndex = 1;
+    event.target.style.transition = "all 0.5s";
+}
+
+function zoomOut(event){
+	event.target.style.transform = "scale(1)";
+    event.target.style.zIndex = 0;
+    event.target.style.transition = "all 0.5s";
+}
+
+function locationQuestion(QId){
+	location.href = '/test/question/read.do?qid=' + QId;
+}
+
+function locationUserProfile(UserId){
+	location.href = '/test/profile?userId=' + UserId;
+}
+
 window.onload = ()=>{
 	headerButton()
 }
